63c2755a7433ccc4db8e43553fffd9dd5b68a4b5
אריה גרוס < @ANDROID-37>
2018-10-25 15:47:18 +0300

    Putted javaDoc and return value

--- src/test.java
+++ src/test.java
@@ -1,10 +1,11 @@
+import java.util.Arrays;
 
 public class test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		easyTest();
-		difficultTest();
+		System.out.println(Arrays.toString(easyTest()));
+		System.out.println(Arrays.toString(difficultTest()));
 //			int v []={4,8,7,1,2,10};
 //			int w []={9,6,3,4,5,2};
 //			int a=15;
@@ -13,20 +14,20 @@ public class test {
 		
 	}
 	
-	public static void easyTest(){
-		int p[] = {2};
-		int w[] = {2};
+	public static int[] easyTest(){
+		int p[] = {2,1};
+		int w[] = {2,1};
 		int t=5;
 		int s=2;
 
-		Knapsack01.N_RESProblem(p,w,s,t); 
+		return Knapsack01.N_RESProblem(p,w,s,t); 
 	}
-	public static void difficultTest(){
-		int p[] = {4,8,7,1,2,10,0};
-		int w[] = {9,6,3,4,5,2,0};
+	public static int[] difficultTest(){
+		int p[] = {4,4,8,7,1,10};
+		int w[] = {9,5,6,3,2,2};
 		int t=5;
-		int s=4;
+		int s=5;
 
-		Knapsack01.N_RESProblem(p,w,s,t);
+		return Knapsack01.N_RESProblem(p,w,s,t);
 	}
 }
