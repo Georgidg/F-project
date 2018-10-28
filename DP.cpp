0d4dc2424b8f736b4af85a2c49cc16f4340b85af
אריה גרוס < @ANDROID-37>
2018-10-28 19:56:02 +0200

    fixes of dp.cpp and better print

--- DP.cpp
+++ DP.cpp
@@ -13,7 +13,8 @@ using namespace std;
      *
      * @return array size n that present which job to do before s with 1 and which after t with 0
      */
-static int* N_RESProblem(unsigned int* p, unsigned int* w,const int len, const unsigned int s, const unsigned int t){
+static int* N_RESProblem(unsigned int* p, unsigned int* w,const int len, const unsigned int s, const unsigned int t,
+                         unsigned int& min){
      unsigned int Mat[len+1][s + 1];
      for (int i = 0; i < len + 1; i++) {
          for (int j = 0; j < s + 1; j++) {
@@ -21,9 +22,9 @@ static int* N_RESProblem(unsigned int* p, unsigned int* w,const int len, const u
          }
      }
      Mat[0][0] = 0;
-     for (int i = 0; i < len - 1; i++) Mat[0][0] += p[i]*w[i];
+     for (int i = 0; i < len; i++) Mat[0][0] += p[i]*w[i];
      unsigned int Ak=0;//The sum of all the weights we have passed
-     for(int i = 0;i < len - 1; i++) {
+     for(int i = 0;i < len; i++) {
          for (int Yk = 0; Yk < s + 1; Yk++) {
              if(Mat[i][Yk] != -1){
                  /////////don't add
@@ -40,16 +41,16 @@ static int* N_RESProblem(unsigned int* p, unsigned int* w,const int len, const u
          Ak+=p[i];
      }
      int * x = new int[len];
-     long min = Mat[len - 1][0];
+     min = Mat[len - 1][0];
      int minIndex = 0;
      for (int i = 1; i < s + 1; i++) {
-         if(min > Mat[len - 1][i] && Mat[len - 1][i] != -1){
-             min =  Mat[len - 1][i];
+         if(min > Mat[len][i] && Mat[len][i] != -1){
+             min =  Mat[len][i];
              minIndex = i;
          }
      }
-     for(int i = len - 2; i >= 0; i--){
-         Ak -= p[i + 1 -1];
+     for(int i = len - 1; i >= 0; i--){
+         Ak -= p[i];
          if(Mat[i][minIndex] + w[i]*(Ak - minIndex) + w[i]*t == Mat[i + 1][minIndex] && Mat[i][minIndex] != -1){
              x[i] = 0;
          }
@@ -61,28 +62,48 @@ static int* N_RESProblem(unsigned int* p, unsigned int* w,const int len, const u
      return x;
  }
 
+void printSolution(unsigned int* p,const int len, const unsigned int t,
+                   unsigned int min,  int* x){
+    unsigned int endTime = 0;
+    cout<<"-------------------------\n";
+    cout<<"befor stoping\n";
+    cout<<"-------------------------\n";
+    for (int i = 0; i < len; ++i) {
+        if(x[i]) {
+            endTime += p[i];
+            cout << "job number: " << i << "  its end time is: " << endTime<<"\n";
+        }
+    }
+    endTime = t;
+    cout<<"-------------------------\n";
+    cout<<"after stoping\n";
+    cout<<"-------------------------\n";
+    for (int i = 0; i < len; ++i) {
+        if(!x[i]) {
+            endTime += p[i];
+            cout << "job number: " << i << " its end time is: " << endTime<<"\n";
+        }
+    }
+    cout << "\nthe Cost function is:  "<<min<<"\n";
+
+}
 int main(){
+    unsigned int min;
     int _n = 2;
     unsigned int _p[] = {2,1};
     unsigned int _w[] = {2,1};
     unsigned int _t=5;
     unsigned int _s=2;
-    int * _x = N_RESProblem(_p, _w, _n, _s, _t);
-    for (int i = 0; i < _n; ++i) {
-        cout << "  "<<_x[i];
-    }
-    cout << "\n";
+    int * _x = N_RESProblem(_p, _w, _n, _s, _t, min);
+    printSolution(_p, _n, _t, min, _x);
     delete [] _x;
     int n = 20;
     unsigned int p[] = {6, 9, 8, 12, 7, 10, 6, 14, 24, 35, 32, 10, 20, 8, 19, 36, 38, 45, 12, 32};
     unsigned int w[] = {22, 24, 21, 21, 12, 16, 9, 10, 17, 21, 17, 5, 10, 4, 9, 16, 12, 14, 2, 3};
     unsigned int s = 76;
     unsigned int t = 153;
-    int* x = N_RESProblem(p, w, n, s, t);
-    for (int i = 0; i < n; ++i) {
-        cout << "  "<<x[i];
-    }
-    cout << "\n";
+    int* x = N_RESProblem(p, w, n, s, t, min);
+    printSolution(p, n, t, min, x);
     delete [] x;
 
 }
